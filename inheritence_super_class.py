edf990c3a52daa17877a79a2dda99df2299e1db9
Varun <varun.prasanan.kurup@avanade.com>
2023-09-30 12:49:14 +0200

    Adding superclass file, and modified inheritance files

--- inheritence_super_class.py
+++ inheritence_super_class.py
@@ -2,6 +2,8 @@ class Pet:
     def __init__(self,name,age):
         self.name=name
         self.age=age
+    
+    def show(self):
         print(f"I am {self.name} and I am {self.age} years old")
     
     def speak(self):
@@ -9,8 +11,15 @@ class Pet:
 
 class Dog(Pet):
 
+    def __init__(self,name,age,colour):
+        super().__init__(name,age)
+        self.colour = colour
+
     def speak(self):
         print("Bark!")
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old and I am {self.colour} in colour")
 
 class Cat(Pet):
     def speak(self):
@@ -21,9 +30,11 @@ class Fish(Pet):
 
 
 p1 = Pet("Manu",34)
-d1 = Dog("Varun", 35)
+d1 = Dog("Varun", 35,"Brown")
 d1.speak()
 c1 = Cat("Arya", 33)
 c1.speak()
 f1 = Fish("Nemo", 3)
 f1.speak()
+d2 = Dog("Maggi",7,"cream")
+d2.show()
